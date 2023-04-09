8c68ecfea503af78fc0f036cba3a3b131c19c4db
Priyanshu Pawar <talk20priyanshu@gmail.com>
2023-04-09 23:42:22 +0530

    day-2

---
+++ Practice/RCT-101/masai-ops-295/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import MobileOs from "./components/MobileOS";
+function App() {
+  return (
+    <div>
+      {/* add MObileOs component here */}
+      <MobileOs />
+    </div>
+  );
+}
+
+export default App;
---
+++ Practice/RCT-101/masai-ops-295/src/components/MobileOS.jsx
@@ -0,0 +1,22 @@
+const MobileOs = () => {
+  return (
+    <div className="MobileApp">
+      <h1>Mobile Operating System</h1>
+      <ul>
+        <li>Android</li>
+        <li>Blackberry</li>
+        <li>iPhone</li>
+        <li>Windows Phone</li>
+      </ul>
+      <h1>Mobile Manufacturers</h1>
+      <ul>
+        <li>Samsung</li>
+        <li>HTC</li>
+        <li>Micromax</li>
+        <li>Apple</li>
+      </ul>
+    </div>
+  );
+};
+
+export default MobileOs;
